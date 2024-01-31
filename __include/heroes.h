0af6bea1b4bb78acf0e828468687e1e72c6f3885
Patrick Lin <bitrush2899@gmail.com>
2024-01-31 23:47:28 +1030

    Converted to UTF-8

--- __include/heroes.h
+++ __include/heroes.h
@@ -16,7 +16,7 @@
 
 
 
-//структуры
+//СЃС‚СЂСѓРєС‚СѓСЂС‹
 typedef struct
 {
    char *name;
@@ -46,13 +46,13 @@ typedef struct
 	char dummy[0x492];
 }HERO;
 */
-// стандартный формат строки по указателю
+// СЃС‚Р°РЅРґР°СЂС‚РЅС‹Р№ С„РѕСЂРјР°С‚ СЃС‚СЂРѕРєРё РїРѕ СѓРєР°Р·Р°С‚РµР»СЋ
 struct _Mes_{
   char *s;
   long  l;  
 };
 
-// стандартный формат выделенной строки по указателю
+// СЃС‚Р°РЅРґР°СЂС‚РЅС‹Р№ С„РѕСЂРјР°С‚ РІС‹РґРµР»РµРЅРЅРѕР№ СЃС‚СЂРѕРєРё РїРѕ СѓРєР°Р·Р°С‚РµР»СЋ
 struct _AMes_{
   _Mes_ m;
   long  a;
@@ -63,12 +63,12 @@ typedef struct
 {
 	int town;
 	int level;
-	char* soundname; // 4 буквы, с которых начинаются звуки монстра
+	char* soundname; // 4 Р±СѓРєРІС‹, СЃ РєРѕС‚РѕСЂС‹С… РЅР°С‡РёРЅР°СЋС‚СЃСЏ Р·РІСѓРєРё РјРѕРЅСЃС‚СЂР°
 	char* defname;
 	int flags;
 	char* name;
 	char* pluralname; 
-	char* features; // (описание способностей)
+	char* features; // (РѕРїРёСЃР°РЅРёРµ СЃРїРѕСЃРѕР±РЅРѕСЃС‚РµР№)
 	int cost_wood;
 	int cost_mercury;
 	int cost_ore;
@@ -126,123 +126,123 @@ typedef struct
 typedef struct{
   Word   x;        //   +00  dw    = x position
   Word   y;        //   +02  dw    = y position
-  Word   l;        //   +04  dw    = ? уровень старшая часть y (y<<2>>C)
-  Byte   Visible;  //   +06  db    = 1 - есть на карте (внутри города или не активен)
+  Word   l;        //   +04  dw    = ? СѓСЂРѕРІРµРЅСЊ СЃС‚Р°СЂС€Р°СЏ С‡Р°СЃС‚СЊ y (y<<2>>C)
+  Byte   Visible;  //   +06  db    = 1 - РµСЃС‚СЊ РЅР° РєР°СЂС‚Рµ (РІРЅСѓС‚СЂРё РіРѕСЂРѕРґР° РёР»Рё РЅРµ Р°РєС‚РёРІРµРЅ)
 //  Byte  _u1[17];
 // +07 db - x
-// +08 db - (?) мусор (к x)
+// +08 db - (?) РјСѓСЃРѕСЂ (Рє x)
 // +09 db - y
-// +0A db - l(?) мусор (к y)
+// +0A db - l(?) РјСѓСЃРѕСЂ (Рє y)
     Dword  PlMapItem; // MixedPos
 // +0B db - (?) l
     Byte  _u1;
-// +0C dd - тип объекта на котором герой стоял
-    long   PlOType; // dd +1E с карты
-// +10 db - бит занятости во флагах поверхности 00001000
-// это бит означающий, что здесь есть/была точка входа (желтая клетка)
+// +0C dd - С‚РёРї РѕР±СЉРµРєС‚Р° РЅР° РєРѕС‚РѕСЂРѕРј РіРµСЂРѕР№ СЃС‚РѕСЏР»
+    long   PlOType; // dd +1E СЃ РєР°СЂС‚С‹
+// +10 db - Р±РёС‚ Р·Р°РЅСЏС‚РѕСЃС‚Рё РІРѕ С„Р»Р°РіР°С… РїРѕРІРµСЂС…РЅРѕСЃС‚Рё 00001000
+// СЌС‚Рѕ Р±РёС‚ РѕР·РЅР°С‡Р°СЋС‰РёР№, С‡С‚Рѕ Р·РґРµСЃСЊ РµСЃС‚СЊ/Р±С‹Р»Р° С‚РѕС‡РєР° РІС…РѕРґР° (Р¶РµР»С‚Р°СЏ РєР»РµС‚РєР°)
     Dword  Pl0Cflag;
-// +14 dd - SetUp с карты
-    Dword  PlSetUp; // dd +0 с карты
-  Word   SpPoints; //   +18  dw    = баллы заклинаний
-  int    Number;   //   +1A  dd    = номер подтипа (конкретный герой)
+// +14 dd - SetUp СЃ РєР°СЂС‚С‹
+    Dword  PlSetUp; // dd +0 СЃ РєР°СЂС‚С‹
+  Word   SpPoints; //   +18  dw    = Р±Р°Р»Р»С‹ Р·Р°РєР»РёРЅР°РЅРёР№
+  int    Number;   //   +1A  dd    = РЅРѕРјРµСЂ РїРѕРґС‚РёРїР° (РєРѕРЅРєСЂРµС‚РЅС‹Р№ РіРµСЂРѕР№)
   Dword  Id;       //   +1E  dd    = Id
-  char   Owner;    //   +22  db    = нозяин (цвет)
-  char   Name[13]; //*   +23  db*D  = имя,0
-  long   Spec;     //   +30  dd    = str[8] str=(*[67CD08])[номер подтипа *5C]
-  Byte   Pic;      //*   +34  db    = номер картинки
+  char   Owner;    //   +22  db    = РЅРѕР·СЏРёРЅ (С†РІРµС‚)
+  char   Name[13]; //*   +23  db*D  = РёРјСЏ,0
+  long   Spec;     //   +30  dd    = str[8] str=(*[67CD08])[РЅРѕРјРµСЂ РїРѕРґС‚РёРїР° *5C]
+  Byte   Pic;      //*   +34  db    = РЅРѕРјРµСЂ РєР°СЂС‚РёРЅРєРё
 // dw +35 ???
 // dw +41 ???
 // db +43 0
   Byte  _u2[15];
     // +3E db -??? 4E3BB5 - used in luck calculation
-  Byte   x0;       //   +44  db    = базовый x для обегания (FF-не ограничен)
-  Byte   y0;       //   +45  db    = базовый y для обегания (FF-не ограничен)
-  Byte   Run;      //   +46  db    = радиус обегания (FF-не ограничен)
+  Byte   x0;       //   +44  db    = Р±Р°Р·РѕРІС‹Р№ x РґР»СЏ РѕР±РµРіР°РЅРёСЏ (FF-РЅРµ РѕРіСЂР°РЅРёС‡РµРЅ)
+  Byte   y0;       //   +45  db    = Р±Р°Р·РѕРІС‹Р№ y РґР»СЏ РѕР±РµРіР°РЅРёСЏ (FF-РЅРµ РѕРіСЂР°РЅРёС‡РµРЅ)
+  Byte   Run;      //   +46  db    = СЂР°РґРёСѓСЃ РѕР±РµРіР°РЅРёСЏ (FF-РЅРµ РѕРіСЂР°РЅРёС‡РµРЅ)
   Byte  _u3;       //   +47  db    = ??? 
   Byte   Flags;    //   +48  8*bb  (463253)
-    // 01 - тип группировки юнитов
-    // 02 - разрешена тактика для героя
-  int    Movement0;//    +49  dd    = полное перемещение начальное
-  int    Movement; //*   +4D  dd    = оставшиеся перемещения
-  Dword  Exp;      //*   +51  dd    = опыт
-  Word   ExpLevel; //*   +55  dw    = уровень
-//  Dword  VStones;  // +57 32 камня (1-посещен,2-нет)
-//  Dword  VMTower;  // +5B Башня Мардетто
-//  Dword  VGarden;  // +5F сад откровения
-//  Dword  VMCamp;   // +63 лагерь наемников
-//  Dword  VSAxis;   // +67 звездное колесо
-//  Dword  VKTree;   // +6B дерево знаний
-//  Dword  VUniver;  // +6F университет
-//  Dword  VArena;   // +73 арена
-//  Dword  VSMagic;  // +77 школа магов
-//  Dword  VSWar;    // +7B школа войны
+    // 01 - С‚РёРї РіСЂСѓРїРїРёСЂРѕРІРєРё СЋРЅРёС‚РѕРІ
+    // 02 - СЂР°Р·СЂРµС€РµРЅР° С‚Р°РєС‚РёРєР° РґР»СЏ РіРµСЂРѕСЏ
+  int    Movement0;//    +49  dd    = РїРѕР»РЅРѕРµ РїРµСЂРµРјРµС‰РµРЅРёРµ РЅР°С‡Р°Р»СЊРЅРѕРµ
+  int    Movement; //*   +4D  dd    = РѕСЃС‚Р°РІС€РёРµСЃСЏ РїРµСЂРµРјРµС‰РµРЅРёСЏ
+  Dword  Exp;      //*   +51  dd    = РѕРїС‹С‚
+  Word   ExpLevel; //*   +55  dw    = СѓСЂРѕРІРµРЅСЊ
+//  Dword  VStones;  // +57 32 РєР°РјРЅСЏ (1-РїРѕСЃРµС‰РµРЅ,2-РЅРµС‚)
+//  Dword  VMTower;  // +5B Р‘Р°С€РЅСЏ РњР°СЂРґРµС‚С‚Рѕ
+//  Dword  VGarden;  // +5F СЃР°Рґ РѕС‚РєСЂРѕРІРµРЅРёСЏ
+//  Dword  VMCamp;   // +63 Р»Р°РіРµСЂСЊ РЅР°РµРјРЅРёРєРѕРІ
+//  Dword  VSAxis;   // +67 Р·РІРµР·РґРЅРѕРµ РєРѕР»РµСЃРѕ
+//  Dword  VKTree;   // +6B РґРµСЂРµРІРѕ Р·РЅР°РЅРёР№
+//  Dword  VUniver;  // +6F СѓРЅРёРІРµСЂСЃРёС‚РµС‚
+//  Dword  VArena;   // +73 Р°СЂРµРЅР°
+//  Dword  VSMagic;  // +77 С€РєРѕР»Р° РјР°РіРѕРІ
+//  Dword  VSWar;    // +7B С€РєРѕР»Р° РІРѕР№РЅС‹
   Dword  Visited[10];
   Byte  _u4[18];   // +7F
-  int    Ct[7];    //   +91  dd*7  = тип существ (-1 - нет)
-  int    Cn[7];    //   +AD  dd*7  = количество
+  int    Ct[7];    //   +91  dd*7  = С‚РёРї СЃСѓС‰РµСЃС‚РІ (-1 - РЅРµС‚)
+  int    Cn[7];    //   +AD  dd*7  = РєРѕР»РёС‡РµСЃС‚РІРѕ
   Byte   SSkill[28];
-	//   +C9  db*1C = уровень 2-х скилов (один байт - уровень этого номера скила 1,2,3) 0-нет
+	//   +C9  db*1C = СѓСЂРѕРІРµРЅСЊ 2-С… СЃРєРёР»РѕРІ (РѕРґРёРЅ Р±Р°Р№С‚ - СѓСЂРѕРІРµРЅСЊ СЌС‚РѕРіРѕ РЅРѕРјРµСЂР° СЃРєРёР»Р° 1,2,3) 0-РЅРµС‚
     // C9=Pathfinding CA=Archery CB=Logistics CC=Scouting CD=Diplomacy CE=Navigation CF=Leadership 
     // D0=Wisdom D1=Mysticism D2=Luck D3=Ballistics D4=Eagle Eye D5=Necromancy D6=Estates D7=Fire Magic 
     // D8=Air Magic D9=Water Magic DA=Earth Magic DB=Scholar DC=Tactics DD=Artillery DE=Learning DF=Offence 
     // E0=Armorer E1=Intelligence E2=Sorcery E3=Resistance E4=First Aid 
-  Byte   SShow[28]; //   +E5  db*1C = порядок отображения 2-х скилов в окне героя (1,2,3,4,5,6)
-  Dword  SSNum;    //   +101 dd    = количество 2-х скилов
+  Byte   SShow[28]; //   +E5  db*1C = РїРѕСЂСЏРґРѕРє РѕС‚РѕР±СЂР°Р¶РµРЅРёСЏ 2-С… СЃРєРёР»РѕРІ РІ РѕРєРЅРµ РіРµСЂРѕСЏ (1,2,3,4,5,6)
+  Dword  SSNum;    //   +101 dd    = РєРѕР»РёС‡РµСЃС‚РІРѕ 2-С… СЃРєРёР»РѕРІ
 //  Word  RefData1;  //   +105  4814D3+...
 //  Word  RefData2;  //   +107  4DA466
-  Dword  TempMod;    // +105 временные модификаторы
-// нач. иниц. при найме 0xFFF9FFFF
-//  00000002 = конющня 3.59 ERM
-//  00000008 = лебединое озеро swan pond 3.59 ERM
-//  00000020 = (???) фонтан удачи fountain of fortune 3.59 ERM
-//  00000080 = оазис
-//  00002000 = домик фей
-//  00040000 = в лодке на воде
-//  00200000 = -3morale в вариор томб
+  Dword  TempMod;    // +105 РІСЂРµРјРµРЅРЅС‹Рµ РјРѕРґРёС„РёРєР°С‚РѕСЂС‹
+// РЅР°С‡. РёРЅРёС†. РїСЂРё РЅР°Р№РјРµ 0xFFF9FFFF
+//  00000002 = РєРѕРЅСЋС‰РЅСЏ 3.59 ERM
+//  00000008 = Р»РµР±РµРґРёРЅРѕРµ РѕР·РµСЂРѕ swan pond 3.59 ERM
+//  00000020 = (???) С„РѕРЅС‚Р°РЅ СѓРґР°С‡Рё fountain of fortune 3.59 ERM
+//  00000080 = РѕР°Р·РёСЃ
+//  00002000 = РґРѕРјРёРє С„РµР№
+//  00040000 = РІ Р»РѕРґРєРµ РЅР° РІРѕРґРµ
+//  00200000 = -3morale РІ РІР°СЂРёРѕСЂ С‚РѕРјР±
 //  00400000 = Give Maximum Luck
 //  00800000 = Give Maximum Moral
-//  38000000 = конкретный тип фонтана удачи
+//  38000000 = РєРѕРЅРєСЂРµС‚РЅС‹Р№ С‚РёРї С„РѕРЅС‚Р°РЅР° СѓРґР°С‡Рё
   Byte  _u6[9];   // +109
   Dword _u7;      // +112 
-  char   DMorale;  // +116 модификаторы морали (накапливаются)
+  char   DMorale;  // +116 РјРѕРґРёС„РёРєР°С‚РѕСЂС‹ РјРѕСЂР°Р»Рё (РЅР°РєР°РїР»РёРІР°СЋС‚СЃСЏ)
   Byte  _u60[3];
-  char   DMorale1; // +11A модиф морали (оазис)
-  char   DLuck;    // +11B модиф удачи до след битвы
+  char   DMorale1; // +11A РјРѕРґРёС„ РјРѕСЂР°Р»Рё (РѕР°Р·РёСЃ)
+  char   DLuck;    // +11B РјРѕРґРёС„ СѓРґР°С‡Рё РґРѕ СЃР»РµРґ Р±РёС‚РІС‹
   Byte  _u6a[17];
-  int   IArt[19][2];  //+12D dd*2*13h = артифакты dd-номер,dd-(FF) (книга 3,FF)
-  Byte  FreeAddSlots;   //+1C5 количество пустых доп. слотов слева
+  int   IArt[19][2];  //+12D dd*2*13h = Р°СЂС‚РёС„Р°РєС‚С‹ dd-РЅРѕРјРµСЂ,dd-(FF) (РєРЅРёРіР° 3,FF)
+  Byte  FreeAddSlots;   //+1C5 РєРѕР»РёС‡РµСЃС‚РІРѕ РїСѓСЃС‚С‹С… РґРѕРї. СЃР»РѕС‚РѕРІ СЃР»РµРІР°
   char  LockedSlot[14]; //+1C6
-  int   OArt[64][2];  //+1D4 dd*2*40 = арт в рюкзаке dd-номер, dd-(FF)
-  Byte    OANum;   //   +3D4 db   = число артифактов в рюкзаке
-  Dword  Sex;      //   +3D5 dd    = пол
-  Byte   fl_B;     //   +3D9 db    = есть биография
-//  char  *Bibl;     //   +3DA dd    -> биография
+  int   OArt[64][2];  //+1D4 dd*2*40 = Р°СЂС‚ РІ СЂСЋРєР·Р°РєРµ dd-РЅРѕРјРµСЂ, dd-(FF)
+  Byte    OANum;   //   +3D4 db   = С‡РёСЃР»Рѕ Р°СЂС‚РёС„Р°РєС‚РѕРІ РІ СЂСЋРєР·Р°РєРµ
+  Dword  Sex;      //   +3D5 dd    = РїРѕР»
+  Byte   fl_B;     //   +3D9 db    = РµСЃС‚СЊ Р±РёРѕРіСЂР°С„РёСЏ
+//  char  *Bibl;     //   +3DA dd    -> Р±РёРѕРіСЂР°С„РёСЏ
 //  Byte  _u7[12];    //  +3DE
     Dword  _5b;     // +3DA
     _AMes_ Bibl;    // +3DE
-  Byte   Spell[70]; //   +3EA db*46 = заклинание (есть/нет)
-  Byte   LSpell[70];//   +430 db*46 = уровень заклинания (>=1)
-  char   PSkill[4]; //   +476 db*4  = первичные навыки
+  Byte   Spell[70]; //   +3EA db*46 = Р·Р°РєР»РёРЅР°РЅРёРµ (РµСЃС‚СЊ/РЅРµС‚)
+  Byte   LSpell[70];//   +430 db*46 = СѓСЂРѕРІРµРЅСЊ Р·Р°РєР»РёРЅР°РЅРёСЏ (>=1)
+  char   PSkill[4]; //   +476 db*4  = РїРµСЂРІРёС‡РЅС‹Рµ РЅР°РІС‹РєРё
   Byte  _u8[24];
 }HERO;
 
 typedef struct {
   Byte   Number;        //* +0 0,1,2,...
   char   Owner;         //*O +1 0,...
-  char   BuiltThisTurn; // +2 - уже строили в этот турн (0-нет, 1-да, 2-не наш город)
+  char   BuiltThisTurn; // +2 - СѓР¶Рµ СЃС‚СЂРѕРёР»Рё РІ СЌС‚РѕС‚ С‚СѓСЂРЅ (0-РЅРµС‚, 1-РґР°, 2-РЅРµ РЅР°С€ РіРѕСЂРѕРґ)
   Byte  _u2;            //* +3 0
   Byte   Type;          //*T +4 0,1...,8
   Byte   x;             //* +5
   Byte   y;             //* +6
   Byte   l;             //* +7
-  Byte   Pos2PlaceBoatX;//* +8 помещать лодку при покупки в Shipyard
+  Byte   Pos2PlaceBoatX;//* +8 РїРѕРјРµС‰Р°С‚СЊ Р»РѕРґРєСѓ РїСЂРё РїРѕРєСѓРїРєРё РІ Shipyard
   Byte   Pos2PlaceBoatY;//* +9
   Byte  _uAa[2];        // +0A
-  int    IHero;         //* +0Ch = номер героя внутри города (-1 - никого нет)
-  int    VHero;         //* +10h = номер героя снаружи города (-1 - никого нет)
-  char   MagLevel;      //*G +14h = уровень магической гильдии в городе (исп. AI для постройки)
+  int    IHero;         //* +0Ch = РЅРѕРјРµСЂ РіРµСЂРѕСЏ РІРЅСѓС‚СЂРё РіРѕСЂРѕРґР° (-1 - РЅРёРєРѕРіРѕ РЅРµС‚)
+  int    VHero;         //* +10h = РЅРѕРјРµСЂ РіРµСЂРѕСЏ СЃРЅР°СЂСѓР¶Рё РіРѕСЂРѕРґР° (-1 - РЅРёРєРѕРіРѕ РЅРµС‚)
+  char   MagLevel;      //*G +14h = СѓСЂРѕРІРµРЅСЊ РјР°РіРёС‡РµСЃРєРѕР№ РіРёР»СЊРґРёРё РІ РіРѕСЂРѕРґРµ (РёСЃРї. AI РґР»СЏ РїРѕСЃС‚СЂРѕР№РєРё)
   Byte  _u15;
-  Word   Monsters[2][7];//*M- +16h ко-лво простых и апгрейднутых
+  Word   Monsters[2][7];//*M- +16h РєРѕ-Р»РІРѕ РїСЂРѕСЃС‚С‹С… Рё Р°РїРіСЂРµР№РґРЅСѓС‚С‹С…
   char  _u32;           //*- +32 = ?
   char  _u33;           //* +33 = 1
   char  _u34;           //* +34 = 0
@@ -251,26 +251,26 @@ typedef struct {
   int   EightMonsterType;           //* +3C = -1
   short EightMonsterAmount;           //*- +40
   Word  _u42;           //  +42
-  int    Spels[5][6];   //*G- +44 сами заклинания
-  char   MagicHild[5];  //*G- +BCh = колво заклинаний в уровне гильдии
+  int    Spels[5][6];   //*G- +44 СЃР°РјРё Р·Р°РєР»РёРЅР°РЅРёСЏ
+  char   MagicHild[5];  //*G- +BCh = РєРѕР»РІРѕ Р·Р°РєР»РёРЅР°РЅРёР№ РІ СѓСЂРѕРІРЅРµ РіРёР»СЊРґРёРё
   Byte  _uC1[3];
   char  _uC4;           //* +C4 = 0
   Byte  _uC5[3];
-  _AMes_ Name;          //*N +C8 -> Имя города
+  _AMes_ Name;          //*N +C8 -> РРјСЏ РіРѕСЂРѕРґР°
   int   _u8[3];         //* +D4 = 0
-  Dword  GuardsT[7];    //*M +E0 = охрана замка
-  Dword  GuardsN[7];    //*M +FC = кол-во охраны
-  Dword  GuardsT0[7];   //*M- +118 = охрана замка
-  Dword  GuardsN0[7];   //*M- +134 = кол-во охраны
-  Byte   Built[8];      //*B +150h = уже построенные здания (0400)
-  Byte   Bonus[8];      //*B +158h = бонус на существ, ресурсы и т.п., вызванный строениями
-  Dword  BMask[2];      //*B- +160h = маска доступных для строения строений
+  Dword  GuardsT[7];    //*M +E0 = РѕС…СЂР°РЅР° Р·Р°РјРєР°
+  Dword  GuardsN[7];    //*M +FC = РєРѕР»-РІРѕ РѕС…СЂР°РЅС‹
+  Dword  GuardsT0[7];   //*M- +118 = РѕС…СЂР°РЅР° Р·Р°РјРєР°
+  Dword  GuardsN0[7];   //*M- +134 = РєРѕР»-РІРѕ РѕС…СЂР°РЅС‹
+  Byte   Built[8];      //*B +150h = СѓР¶Рµ РїРѕСЃС‚СЂРѕРµРЅРЅС‹Рµ Р·РґР°РЅРёСЏ (0400)
+  Byte   Bonus[8];      //*B +158h = Р±РѕРЅСѓСЃ РЅР° СЃСѓС‰РµСЃС‚РІ, СЂРµСЃСѓСЂСЃС‹ Рё С‚.Рї., РІС‹Р·РІР°РЅРЅС‹Р№ СЃС‚СЂРѕРµРЅРёСЏРјРё
+  Dword  BMask[2];      //*B- +160h = РјР°СЃРєР° РґРѕСЃС‚СѓРїРЅС‹С… РґР»СЏ СЃС‚СЂРѕРµРЅРёСЏ СЃС‚СЂРѕРµРЅРёР№
 }CASTLE;
 
 
 #pragma pack (pop)
 
-//константы
+//РєРѕРЅСЃС‚Р°РЅС‚С‹
 #define CREATURE_CHAMPION		11
 
 
@@ -458,7 +458,7 @@ typedef struct {
 #define HOOK_SIZE 5
 
 
-//переменные
+//РїРµСЂРµРјРµРЅРЅС‹Рµ
 /*
 #define CONST_advManager	(void*)(*(int*)0x6992B8)
 
@@ -471,17 +471,17 @@ typedef struct {
 #define grail_cnt
 */
 
-//функции
+//С„СѓРЅРєС†РёРё
 extern int (__thiscall *HasArtifact)(void *hero, int art);
-		//проверка на наличие артефакта
+		//РїСЂРѕРІРµСЂРєР° РЅР° РЅР°Р»РёС‡РёРµ Р°СЂС‚РµС„Р°РєС‚Р°
 
 extern int (__thiscall *HasArtifactInBackpack)(void *hero, int art);
 
 extern int (__thiscall *HasCreature)(void *hero, int creature);
-		//проверка на наличие существа
+		//РїСЂРѕРІРµСЂРєР° РЅР° РЅР°Р»РёС‡РёРµ СЃСѓС‰РµСЃС‚РІР°
 
 extern int(__thiscall *ShowGrailMap)(void *advManager);
-		//показать граальную карту
+		//РїРѕРєР°Р·Р°С‚СЊ РіСЂР°Р°Р»СЊРЅСѓСЋ РєР°СЂС‚Сѓ
 
 extern int(__thiscall *ShowThiefGuild)(void *_this, int amount);
 
@@ -490,10 +490,10 @@ extern int(__thiscall *RandArtGenerator)(int _this, int _class);
 extern char(__thiscall *sub_5A43E0)(void* combatManager, int a2, int a3, int a4, int a5, int a6);
 	
 extern int (__thiscall *CastBattleSpell)(void* combatManager, int spell, signed int target, unsigned int source, int a5, int mastery, int spellpower);
-		//каст спелла в бою
+		//РєР°СЃС‚ СЃРїРµР»Р»Р° РІ Р±РѕСЋ
 
 extern int (__thiscall *SummonCreature)(void *combat_manager, int spell, int creature, int spellpower, int unk);
-      //вызов существа спеллом
+      //РІС‹Р·РѕРІ СЃСѓС‰РµСЃС‚РІР° СЃРїРµР»Р»РѕРј
 
 extern int (__fastcall *ShowMSG)(const char *text, int type, int f1, int f2, int f3, int f4, int f5, int f6, int f7, int f8, int f9, int f10);
 
@@ -501,7 +501,7 @@ extern void (__cdecl *CallERM)(int num);
 
 extern void (_cdecl  *hprintf)(int, char*, ...);
 
-//переходники
+//РїРµСЂРµС…РѕРґРЅРёРєРё
 extern void* HeroStructPointer(char hero);
 
 extern int* BattleStackParam(int stack, int param_offset);
